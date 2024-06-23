df49affb2bb3e7b48b71c0b285cbdd78e169b68e
Isaayyy <124240745+Isaayyy@users.noreply.github.com>
2024-06-23 09:37:17 +0800

    Delete LE2 directory

--- LE2/BlogDataLibrary/Database/ISqlDataAccess.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace BlogDataLibrary.Database
-{
-    public interface ISqlDataAccess
-    {
-        List<T> LoadDatae<T, U>(string sqlStatement, U parameters, string connectionStringName, bool isStoredProcedure);
-        void SaveData<T>(string sqlStatement, T parameters, string connectionStringName, bool isStoredProcedure);
-    }
-}
