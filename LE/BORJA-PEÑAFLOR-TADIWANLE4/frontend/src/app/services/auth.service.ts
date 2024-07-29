f5f66bd535aab7a50eb9b3226fd5a527d7c4ed24
Isaayyy <luisamaepenaflor@gmail.com>
2024-07-29 12:06:02 +0800

    le5

--- LE/BORJA-PEÑAFLOR-TADIWANLE4/frontend/src/app/services/auth.service.ts
+++ LE/BORJA-PEÑAFLOR-TADIWANLE4/frontend/src/app/services/auth.service.ts
@@ -1,18 +1,27 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   isLoggedIn: boolean = false;
-  public redirectUrl: string = "";
+  public redirectUrl: string = "/posts";
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient) { }
 
+  login(username: string, password: string): Observable<LoginPostData> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+
+    return this.http.post<LoginPostData>('https://localhost:7110/api/Login/login', { username, password }, { headers });
   }
+}
 
-  login(username: string, password: string) {
-    return this.http.post<string>("https://localhost:7110/api/Login/login", { username, password });
-  }
+export interface LoginPostData {
+  id_token: string;
+  id: number;
 }
